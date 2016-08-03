7b46021c98d25f93f2cf78f98430520d48052319
Michael B <dev@mihael.me>
2016-08-03 12:45:49 +0300

    implement all tests

--- tests/ImageLoaderTest.php
+++ tests/ImageLoaderTest.php
@@ -25,4 +25,47 @@ class ImageLoaderTest extends TestCase {
         }
     }
 
+    public function testNotImage() {
+        $downloadPath = __DIR__ . '/cover_letter.jpg';
+        try {
+            //actually it's pdf ;)
+            $loader = new ImageLoader('http://mihael.me/sites/default/files/cover_letter.jpg');
+            $loader->download($downloadPath);
+            unlink($downloadPath);
+            $this->fail('File is not an image not thrown');
+        } catch(\Exception $e) {
+            unlink($downloadPath);
+            $this->assertEquals('File is not an image', $e->getMessage());
+        }
+    }
+
+    public function testWrongMimeType() {
+        $downloadPath = __DIR__ . '/test.jpg';
+
+        try {
+            //actually it's bmp ;)
+            $loader = new ImageLoader('http://mihael.me/sites/default/files/test.jpg');
+            $loader->download($downloadPath);
+            unlink($downloadPath);
+            $this->fail('Image has wrong mime type');
+        } catch (\Exception $e) {
+            unlink($downloadPath);
+            $this->assertEquals('Image has wrong mime type', $e->getMessage());
+        }
+    }
+
+    public function testSuccessfulDownload() {
+        $downloadPath = __DIR__ . '/face.jpg';
+        try {
+            //huh, this is real image
+            $loader = new ImageLoader('http://mihael.me/sites/default/files/large_1KCapF6H02c.jpg');
+            $return = $loader->download($downloadPath);
+            unlink($downloadPath);
+            $this->assertEquals($return, true);
+        } catch(\Exception $e) {
+            unlink($downloadPath);
+            $this->fail($e->getMessage());
+        }
+    }
+
 }
